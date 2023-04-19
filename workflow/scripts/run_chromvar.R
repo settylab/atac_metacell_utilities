bf5bf6020f0934f0e89f0fef5d00c44f31fc9588
Christine Dien <cdien@fredhutch.org>
2023-04-19 14:56:31 -0700

    update config, snakefile, and dag image

--- workflow/scripts/run_chromvar.R
+++ workflow/scripts/run_chromvar.R
@@ -7,13 +7,14 @@ library(SummarizedExperiment)
 library(BiocParallel)
 
 peak_file <- args[1]
-chromvar_dir <- args[2]
+chromvar_indir <- args[2]
+chromvar_outdir <- args[3]
 
 peaks <- getPeaks(peak_file, sort_peaks=TRUE, extra_cols=c('summit'=4, 'score'=5,'name'=6))
-count_matrix <- Matrix::readMM(sprintf("%s/sc_atac_counts.mtx", chromvar_dir))
+count_matrix <- Matrix::readMM(sprintf("%s/sc_atac_counts.mtx", chromvar_indir))
 count_matrix <- t(count_matrix)
 
-nfrags <- readr::read_csv(sprintf("%s/sc_nfrags.csv", chromvar_dir))
+nfrags <- readr::read_csv(sprintf("%s/sc_nfrags.csv", chromvar_indir))
 col_data <- DataFrame(nfrags$nFrags, row.names=nfrags$x)
 names(col_data) <- 'depth'
 
@@ -21,8 +22,8 @@ fragment_counts <- SummarizedExperiment(assays=list(counts=count_matrix), rowRan
 fragment_counts <- addGCBias(fragment_counts, genome=BSgenome.Hsapiens.UCSC.hg38)
 counts_filtered <- filterPeaks(fragment_counts, non_overlapping=TRUE)
 
-tf_names <- readr::read_csv(sprintf("%s/chromvar_tf_names.csv", chromvar_dir))
-insilico_chip_mat <- Matrix::readMM(sprintf("%s/bin_ins_chip.mtx", chromvar_dir))
+tf_names <- readr::read_csv(sprintf("%s/chromvar_tf_names.csv", chromvar_indir))
+insilico_chip_mat <- Matrix::readMM(sprintf("%s/bin_ins_chip.mtx", chromvar_indir))
 
 anno <- getAnnotations(insilico_chip_mat, rowRanges=rowRanges(counts_filtered))
 colnames(anno) <-tf_names$tf_name
@@ -33,6 +34,6 @@ dev <- computeDeviations(object=counts_filtered, annotations=anno, background_pe
 # export output
 dir.create(sprintf("%s/export", chromvar_dir))
 deviations <- assay(dev, "deviations")
-write.csv(deviations, sprintf("%s/output/deviations.csv", chromvar_dir))
+write.csv(deviations, sprintf("%s/deviations.csv", chromvar_outdir))
 zs_deviations <- assay(dev, "z")
-write.csv(zs_deviations, sprintf("%s/output/deviations_zs.csv", chromvar_dir))
+write.csv(zs_deviations, sprintf("%s/deviations_zs.csv", chromvar_outdir))
